09ab123be2e7af9fcf8be7b1f04bd3475635731f
JoanaaAlves <estt14055@ipt.pt>
2026-01-24 16:23:45 +0000

    ajustes e alteracoes

--- app/src/main/java/pt/ipt/dam/projfinal/cam.kt
+++ app/src/main/java/pt/ipt/dam/projfinal/cam.kt
@@ -1,37 +1,46 @@
 package pt.ipt.dam.projfinal
 
 import android.Manifest
+import android.content.ContentValues
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
+import android.provider.MediaStore
 import android.util.Log
+import android.widget.Button
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.result.contract.ActivityResultContracts
-import androidx.annotation.OptIn
 import androidx.appcompat.app.AppCompatActivity
-import androidx.camera.core.*
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.ImageCapture
+import androidx.camera.core.ImageCaptureException
+import androidx.camera.core.Preview
 import androidx.camera.lifecycle.ProcessCameraProvider
 import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import com.google.mlkit.vision.barcode.BarcodeScanning
-import com.google.mlkit.vision.common.InputImage
 import pt.ipt.dam.projfinal.databinding.ActivityCamBinding
-import java.util.concurrent.ExecutorService
+import java.text.SimpleDateFormat
+import java.util.Locale
 import java.util.concurrent.Executors
-
+import java.util.concurrent.ExecutorService
+import kotlin.collections.contains
 class cam : AppCompatActivity() {
 
+
     private lateinit var binding: ActivityCamBinding
+    private lateinit var imageCapture: ImageCapture
     private lateinit var cameraExecutor: ExecutorService
-    private var isScanning = true // Para evitar abrir a nova página várias vezes
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         binding = ActivityCamBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        // setContentView(R.layout.activity_main)
 
         ViewCompat.setOnApplyWindowInsetsListener(binding.root) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
@@ -39,113 +48,189 @@ class cam : AppCompatActivity() {
             insets
         }
 
-        cameraExecutor = Executors.newSingleThreadExecutor()
-
+        // pedir permissão para usar a câmara
         if (allPermissionsGranted()) {
             startCamera()
         } else {
             requestPermissions()
         }
 
-        binding.btnvoltar.setOnClickListener {
-            startActivity(Intent(this, MainActivity::class.java))
-            finish()
+
+        binding.imageCaptureButton.setOnClickListener {
+            takePhoto()
         }
-    }
 
-    private fun startCamera() {
-        val cameraProviderFuture = ProcessCameraProvider.getInstance(this)
+        // configura acesso à câmara
+        // utiliza o padrão 'Singleton'
+        cameraExecutor = Executors.newSingleThreadExecutor()
 
-        cameraProviderFuture.addListener({
-            val cameraProvider: ProcessCameraProvider = cameraProviderFuture.get()
 
-            // 1. Preview
-            val preview = Preview.Builder().build().also {
-                it.setSurfaceProvider(binding.viewFinder.surfaceProvider)
-            }
 
-            // 2. Image Analysis (Onde o QR é lido)
-            val imageAnalyzer = ImageAnalysis.Builder()
-                .setBackpressureStrategy(ImageAnalysis.STRATEGY_KEEP_ONLY_LATEST)
-                .build()
-                .also {
-                    it.setAnalyzer(cameraExecutor) { imageProxy ->
-                        processImageProxy(imageProxy)
-                    }
-                }
+        val btnVoltar = findViewById<Button>(R.id.btnvoltar)
 
-            val cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
+        btnVoltar.setOnClickListener {
 
-            try {
-                cameraProvider.unbindAll()
-                cameraProvider.bindToLifecycle(
-                    this, cameraSelector, preview, imageAnalyzer
-                )
-            } catch (exc: Exception) {
-                Log.e("CameraX", "Use case binding failed", exc)
-            }
-        }, ContextCompat.getMainExecutor(this))
-    }
+            //intent para na class main
 
-    @OptIn(ExperimentalGetImage::class)
-    private fun processImageProxy(imageProxy: ImageProxy) {
-        val mediaImage = imageProxy.image
-        if (mediaImage != null && isScanning) {
-            val image = InputImage.fromMediaImage(mediaImage, imageProxy.imageInfo.rotationDegrees)
-            val scanner = BarcodeScanning.getClient()
+            val intent = Intent(this, MainActivity::class.java)
 
-            scanner.process(image)
-                .addOnSuccessListener { barcodes ->
-                    for (barcode in barcodes) {
-                        val rawValue = barcode.rawValue ?: ""
-                        val lines = rawValue.lines()
-                        val firstLine = lines.getOrNull(0)?.trim()
-                        val secondLine = lines.getOrNull(1)?.trim()
+            // Começar a activity
+            startActivity(intent)
 
-                        if (firstLine == "HASH:hashsegura") {
-                            isScanning = false // Para a leitura
 
-                            val intent = Intent(this, lerqrcode::class.java)
-                            intent.putExtra("url", secondLine)
-                            startActivity(intent)
-                            finish()
-                            break
-                        }
-                    }
-                }
-                .addOnCompleteListener {
-                    imageProxy.close() // Fecha o frame para processar o próximo
-                }
-        } else {
-            imageProxy.close()
         }
-    }
+    } // fim onCreate()
 
-    // --- Gestão de Permissões ---
+    /**
+     * ask for permissions
+     */
     private fun requestPermissions() {
         activityResultLauncher.launch(REQUIRED_PERMISSIONS)
     }
 
+    /**
+     * define if all permissions has been granted
+     */
     private fun allPermissionsGranted() = REQUIRED_PERMISSIONS.all {
-        ContextCompat.checkSelfPermission(baseContext, it) == PackageManager.PERMISSION_GRANTED
+        ContextCompat.checkSelfPermission(
+            baseContext, it
+        ) == PackageManager.PERMISSION_GRANTED
     }
 
-    private val activityResultLauncher = registerForActivityResult(
-        ActivityResultContracts.RequestMultiplePermissions()
-    ) { permissions ->
-        if (permissions[Manifest.permission.CAMERA] == true) {
-            startCamera()
-        } else {
-            Toast.makeText(this, "Permissão negada", Toast.LENGTH_SHORT).show()
+
+    /**
+     * if it is necessary to ask for permissions,
+     * this will evaluate the answers provided by user
+     * and start the camera, or inform user that it can not use the camera
+     */
+    private val activityResultLauncher =
+        registerForActivityResult(
+            ActivityResultContracts.RequestMultiplePermissions()
+        )
+        { permissions ->
+
+            // Handle Permission granted/rejected
+            var permissionGranted = true
+            permissions.entries.forEach {
+                // test for all types of permissions
+                if (it.key in REQUIRED_PERMISSIONS && !it.value)
+                    permissionGranted = false
+            }
+            if (!permissionGranted) {
+                // ver também 'SnackBar'
+                Toast.makeText(
+                    baseContext,
+                    getString(R.string.permission_denied),
+                    Toast.LENGTH_SHORT
+                ).show()
+            } else {
+                startCamera()
+            }
         }
+
+    /**
+     * start the camera
+     */
+    private fun startCamera() {
+        val cameraProviderFuture = ProcessCameraProvider.getInstance(this)
+
+        cameraProviderFuture.addListener({
+            // Used to bind the lifecycle of cameras to the lifecycle owner
+            val cameraProvider: ProcessCameraProvider = cameraProviderFuture.get()
+
+            // Preview
+            val preview = Preview.Builder()
+                .build()
+                .also {
+                    it.setSurfaceProvider(binding.viewFinder.surfaceProvider)
+                }
+
+            imageCapture = ImageCapture.Builder().build()
+
+            // Select back camera as a default
+            val cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
+
+            try {
+                // Unbind use cases before rebinding
+                cameraProvider.unbindAll()
+
+                // Bind use cases to camera
+                cameraProvider.bindToLifecycle(
+                    this, cameraSelector, preview, imageCapture
+                )
+            } catch (exc: Exception) {
+                Log.e(TAG, "Use case binding failed", exc)
+            }
+
+        }, ContextCompat.getMainExecutor(this))
     }
+
+
+    /**
+     * function to take the photo and save it to storage
+     * it must define the name and location of photo
+     */
+    private fun takePhoto() {
+        // Get a stable reference of the modifiable image capture use case
+        val imageCapture = imageCapture ?: return
+
+        // Create a time stamped name
+        // import java.text.SimpleDateFormat
+        val name = SimpleDateFormat(FILENAME_FORMAT, Locale.US).format(System.currentTimeMillis())
+        // define the type of image, and where it should be stored
+        val contentValues = ContentValues().apply {
+            put(MediaStore.MediaColumns.DISPLAY_NAME, name)
+            put(MediaStore.MediaColumns.MIME_TYPE, "image/jpeg")
+            if (Build.VERSION.SDK_INT > Build.VERSION_CODES.P) {
+                // Android version is higher than 9 (Android 9 (Pie) --> SDK_INT = 28)
+                put(MediaStore.Images.Media.RELATIVE_PATH, "Pictures/CameraX-Images")
+            }
+        }
+
+        // Create output options object which contains file + metadata
+        val outputOptions = ImageCapture.OutputFileOptions.Builder(
+            contentResolver, MediaStore.Images.Media.EXTERNAL_CONTENT_URI, contentValues
+        ).build()
+
+        // Set up image capture listener, which is triggered after photo has
+        // been taken
+        imageCapture.takePicture(
+            outputOptions,
+            ContextCompat.getMainExecutor(this),
+            object : ImageCapture.OnImageSavedCallback {
+                override fun onError(exc: ImageCaptureException) {
+                    Log.e(TAG, "Photo capture failed: ${exc.message}", exc)
+                }
+
+                override fun onImageSaved(output: ImageCapture.OutputFileResults) {
+                    val msg = getString(R.string.after_take_photo,output.savedUri)
+                    Toast.makeText(baseContext, msg, Toast.LENGTH_LONG).show()
+                    Log.d(TAG, msg)
+                }
+            })
+    }
+
 
     override fun onDestroy() {
         super.onDestroy()
         cameraExecutor.shutdown()
     }
 
+
     companion object {
-        private val REQUIRED_PERMISSIONS = arrayOf(Manifest.permission.CAMERA)
+        private const val TAG = "CameraXApp"
+        private const val FILENAME_FORMAT = "yyyy-MM-dd-HH-mm-ss-SSS"
+        private val REQUIRED_PERMISSIONS =
+            mutableListOf(
+                // import android.Manifest
+                Manifest.permission.CAMERA
+            ).apply {
+                if (Build.VERSION.SDK_INT <= Build.VERSION_CODES.P) {
+                    // check if Android version is 9 (Pie, API 28) or lower
+                    add(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                }
+            }.toTypedArray()
     }
+
+
 }
